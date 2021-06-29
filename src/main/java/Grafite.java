d882c63cd89292e0561b6b47ac6a2aea690d8cdd
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2021-06-29 20:18:20 -0300

    Atualização do código

--- src/main/java/Grafite.java
+++ src/main/java/Grafite.java
@@ -1,36 +1,49 @@
 public class Grafite {
-
-
+    private int tamanho;
+    private Grafite.Dureza dureza;
+    private float calibre;
 
     public enum Dureza {
-        G_HB,
-        G_2B,
-        G_4B,
-        G_6B;
+        G_HB(1),
+        G_2B(2),
+        G_4B(4),
+        G_6B(6);
 
+        int desgastePorFolha;
+        Dureza(int desgastePorFolha){
+            this.desgastePorFolha = desgastePorFolha;
+        }
+        
+        public int getDesgastePorFolha(){
+            return desgastePorFolha;
+        }
+    
     }
 
-
     public Grafite(float calibre, Dureza dureza, int tamanho) {
+        this.calibre = calibre;
+        this.dureza = dureza;
+        this.tamanho = tamanho;
     }
 
     public int desgastePorFolha() {
-        return -1;
+        return dureza.getDesgastePorFolha();
     }
 
     public Dureza getDureza() {
-        return null;
+        return dureza;
     }
 
     public float getCalibre() {
-        return -1f;
+        return calibre;
     }
 
     public int getTamanho() {
-        return -1;
+        return tamanho;
     }
 
-    public void setTamanho(int tamanho) { }
-
-
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
 }
+
--- src/main/java/Lapiseira.java
+++ src/main/java/Lapiseira.java
@@ -1,32 +1,74 @@
 public class Lapiseira {
-
+    private float calibre;
+    private Grafite grafite;
+    private int folhasEscritas;
+    private int podeSerEscritas;
 
     public Lapiseira(float calibre) {
-
+        this.calibre = calibre;
     }
 
     public boolean inserir(Grafite grafite) {
-        return false;
+        if(getGrafite() != null){
+            return false;    
+        }else{
+            if(grafite.getCalibre() == this.calibre){
+                this.grafite = grafite;
+                folhasEscritas = 0;
+                setPodeSerEscritas();
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 
     public boolean remover() {
-
-        return false;
+        if(getGrafite() == null){
+            return false;
+        }else{
+            this.grafite = null;
+            return true;
+        }        
     }
 
     public boolean escrever(int folhas) {
-        return false;
+        if(getGrafite() == null){
+            return false;
+        }else{
+            if(podeSerEscritas > folhas){
+                folhasEscritas += folhas;
+                podeSerEscritas -= folhas;
+                return true;
+            }else if(podeSerEscritas < folhas){
+                folhasEscritas += podeSerEscritas;
+                remover();
+                return false;
+            }else{
+                folhasEscritas += podeSerEscritas;
+                remover();
+                return true;
+            }
+        }
     }
 
     public Grafite getGrafite() {
-        return null;
+        return grafite;
     }
 
     public float getCalibre() {
-        return -1f;
+        return calibre;
     }
 
     public int getFolhasEscritas() {
-        return -1;
+        return folhasEscritas;
+    }
+
+    public void setPodeSerEscritas() {
+        podeSerEscritas = grafite.getTamanho() / grafite.getDureza().getDesgastePorFolha();
+    }
+
+    public int getPodeSerEscritas() {
+        return podeSerEscritas;    
     }
 }
